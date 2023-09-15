62333ad8706c8d6d3ae0c308ed7fc8c6eafdbaed
bariskarabela <Kekre255013>
2023-09-15 17:22:33 +0300

    Add project files.

---
+++ CleanArchitecture.WebApi/Program.cs
@@ -0,0 +1,28 @@
+using CleanArchitecture.WebApi.Configurations;
+using CleanArchitecture.WebApi.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .InstallServices(
+    builder.Configuration,
+    builder.Host,
+    typeof(IServiceInstaller).Assembly);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors();
+
+app.UseMiddlewareExtensions();
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
